4edd1cfb6c1cc3e68713b96ca8f528d4f55770ce
Leandro Souza <leandrosouza6374@gmail.com>
2024-09-14 23:14:29 -0300

    dd

--- pegarArrayComBaseNoCode.js
+++ pegarArrayComBaseNoCode.js
@@ -1,6 +1,18 @@
 function pegarArrayComBaseNoCode() {
   const result = array.filter((item) => item.startsWith(code1));
-  console.log(result);
+  console.log("PRÓXIMA : " + result[0]);
   console.log(result.length);
   document.getElementById(`chancesDeGanhar`).innerHTML = result.length;
+
+  // Verifica se 'result' e 'code1' são strings
+  if (typeof result[0] === "string" && typeof code1 === "string") {
+    // Filtra os caracteres que estão em posições diferentes ou que não existem em code1
+    const diff = result[0]
+      .split("")
+      .filter((char, index) => code1[index] !== char)
+      .join("");
+    console.log("DIFERENÇA : " + diff); // Retorna a diferença
+  } else {
+    console.log("Erro: result e code1 devem ser strings");
+  }
 }
